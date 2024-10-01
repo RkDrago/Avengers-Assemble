9e5863825374840118324bc7760a25ede8de2b7a
RkDrago <bolopandu786@gmail.com>
2024-10-01 17:52:25 +0530

    9th commit

--- app/contact/page.js
+++ app/contact/page.js
@@ -33,18 +33,18 @@ const Contact = () => {
                             <form className="contact-form flex flex-col gap-3 relative">
                                 <div className="form-group flex flex-col gap-1 z-10">
                                     <label className='font-bold pl-1' htmlFor="name">Name:</label>
-                                    <input className='bg-[#98e38e8a] focus:bg-[#98e38eaa] text-black font-bold rounded h-[2rem] px-2 placeholder-[#11111173]' type="text" id="name" placeholder='Enter your full name' name="name" required />
+                                    <input className='bg-[#3d3d3daa] focus:bg-[#6a6a6aaa] text-white font-bold rounded h-[2rem] px-2 placeholder-[#92929256]' type="text" id="name" placeholder='Enter your full name' name="name" required />
                                 </div>
                                 <div className="form-group flex flex-col gap-1 z-10">
                                     <label className='font-bold pl-1' htmlFor="email">Email:</label>
-                                    <input className='bg-[#98e38e8a] focus:bg-[#98e38eaa] text-black font-bold rounded h-[2rem] px-2 placeholder-[#11111173]' type="email" id="email" placeholder='Enter your email address' name="email" required />
+                                    <input className='bg-[#3d3d3daa] focus:bg-[#6a6a6aaa] text-white font-bold rounded h-[2rem] px-2 placeholder-[#92929256]' type="email" id="email" placeholder='Enter your email address' name="email" required />
                                 </div>
                                 <div className="form-group flex flex-col gap-1 z-10">
                                     <label className='font-bold pl-1' htmlFor="message">Message:</label>
-                                    <textarea className='bg-[#98e38e8a] focus:bg-[#98e38eaa] text-black font-bold rounded min-h-[4rem] h-[4rem] max-h-[8rem] px-2 placeholder-[#11111173]' id="message" placeholder='Write your message here...' name="message" rows="5" required></textarea>
+                                    <textarea className='bg-[#3d3d3daa] focus:bg-[#6a6a6aaa] text-white font-bold rounded min-h-[4rem] h-[4rem] max-h-[8rem] px-2 placeholder-[#92929256]' id="message" placeholder='Write your message here...' name="message" rows="5" required></textarea>
                                 </div>
-                                <button type="submit" className="submit-button absolute -right-[5rem] bottom-0 bg-[#98e38e8a] hover:bg-[#98e38eaa] rounded-full h-[4rem] w-[4rem] flex justify-center items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" color="#111" fill="none">
+                                <button type="submit" className="submit-button absolute -right-[5rem] bottom-0 bg-[#3d3d3daa] hover:bg-[#6a6a6aaa] rounded-full h-[4rem] w-[4rem] flex justify-center items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" color="#929292" fill="none">
                                         <path d="M22 12.5001C22 12.0087 21.9947 11.0172 21.9842 10.5244C21.9189 7.45886 21.8862 5.92609 20.7551 4.79066C19.6239 3.65523 18.0497 3.61568 14.9012 3.53657C12.9607 3.48781 11.0393 3.48781 9.09882 3.53656C5.95033 3.61566 4.37608 3.65521 3.24495 4.79065C2.11382 5.92608 2.08114 7.45885 2.01576 10.5244C1.99474 11.5101 1.99475 12.4899 2.01577 13.4756C2.08114 16.5412 2.11383 18.0739 3.24496 19.2094C4.37608 20.3448 5.95033 20.3843 9.09883 20.4634C9.90159 20.4836 10.7011 20.4954 11.5 20.4989" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                                         <path d="M2 6L8.91302 9.92462C11.4387 11.3585 12.5613 11.3585 15.087 9.92462L22 6" stroke="currentColor" stroke-width="3" stroke-linejoin="round" />
                                         <path d="M22 17.5L14 17.5M22 17.5C22 16.7998 20.0057 15.4915 19.5 15M22 17.5C22 18.2002 20.0057 19.5085 19.5 20" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
