4d73e96206caa7754a4cc20f5ebd72cd59c8c98b
RkDrago <bolopandu786@gmail.com>
2024-10-15 23:36:06 +0530

    23th commit

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "The Avengers Initiative: Heroes United",
   description: "Explore Earth's mightiest heroes and their legendary battle for justice.",
+  "google-site-verification": "MFgZAXriGtgNcUi1H0Ba1m5GIi1BVpARHHExZi9uyYk",
 };
 
 export default function RootLayout({ children }) {
