aa46a3328bc9db3d37cc187e4ff31722bf7bf8fc
RkDrago <bolopandu786@gmail.com>
2024-10-16 00:02:01 +0530

    25th commit

--- app/layout.js
+++ app/layout.js
@@ -6,12 +6,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "The Avengers Initiative: Heroes United",
   description: "Explore Earth's mightiest heroes and their legendary battle for justice.",
-  "google-site-verification": "MFgZAXriGtgNcUi1H0Ba1m5GIi1BVpARHHExZi9uyYk",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="MFgZAXriGtgNcUi1H0Ba1m5GIi1BVpARHHExZi9uyYk" />
+      </head>
       <body className={inter.className}>
         {children}
       </body>
