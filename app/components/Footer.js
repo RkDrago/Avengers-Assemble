891d34f9852ba8894c6f6700e7cb4fd191e4a14c
RkDrago <bolopandu786@gmail.com>
2025-03-28 15:08:54 +0530

    11th commit

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
     <>
       <div className="h-[3.1rem] w-[100svw] absolute bottom-0 overflow-hidden z-20">
         <div className='designer-pentagon text-white text-center pt-11 pb-3 bg-[#4CAF50] m-auto'>
-          <h1 className='scale-y-[-1] text-black font-thin sans'>@2024 Designed with ❤️ by RkDrago</h1> 
+          <h1 className='scale-y-[-1] text-black font-thin sans'>@{currentYear} Designed with ❤️ by RkDrago</h1> 
         </div>
       </div>
     </>
